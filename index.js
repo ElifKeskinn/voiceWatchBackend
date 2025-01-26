ab96062279de33f6b5ec679de4370e5c995d07c9
Elif <elif.keskin233@gmail.com>
2025-01-26 15:54:15 +0300

    port changed

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ db.sequelize.authenticate()
   })
   .then(() => {
     console.log('Database synchronized...');
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => {
