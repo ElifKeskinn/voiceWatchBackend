b0df8f503145bf1c8e64248358bb6516331712a2
Elif <elif.keskin233@gmail.com>
2025-01-30 14:22:43 +0300

    changes backwards

--- models/index.js
+++ models/index.js
@@ -1,42 +1,46 @@
-'use strict';
+const { Sequelize } = require('sequelize');
+const config = require('../config/config').development;
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname, '..', 'config', 'config.js'))[env];
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  {
+    host: config.host,
+    dialect: config.dialect,
+    dialectOptions: config.dialectOptions,
+    logging: config.logging,
+  }
+);
+
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// Modelleri yükle
+db.User = require('./user')(sequelize, Sequelize);
+db.Contact = require('./contact')(sequelize, Sequelize);
+db.PasswordReset = require('./passwordReset')(sequelize, Sequelize);
+db.Alert = require('./alert')(sequelize, Sequelize);
+db.AlertResponse = require('./alertResponse')(sequelize, Sequelize);
+
+// İlişkileri tanımla
+// EmergencyContacts
+db.User.hasMany(db.Contact, { as: 'emergencyContacts', foreignKey: 'userId', onDelete: 'NO ACTION' });
+db.Contact.belongsTo(db.User, { foreignKey: 'userId', onDelete: 'NO ACTION' });
+
+// PasswordResets
+db.User.hasMany(db.PasswordReset, { as: 'passwordResets', foreignKey: 'userId', onDelete: 'NO ACTION' });
+db.PasswordReset.belongsTo(db.User, { foreignKey: 'userId', as: 'user', onDelete: 'NO ACTION' });
+
+// Alerts
+db.User.hasMany(db.Alert, { as: 'alerts', foreignKey: 'userId', onDelete: 'NO ACTION' });
+db.Alert.belongsTo(db.User, { foreignKey: 'userId', as: 'user', onDelete: 'NO ACTION' });
+
+db.Alert.hasMany(db.AlertResponse, { as: 'responses', foreignKey: 'alertId', onDelete: 'NO ACTION' });
+db.AlertResponse.belongsTo(db.Alert, { foreignKey: 'alertId', as: 'alert', onDelete: 'NO ACTION' });
+
+db.AlertResponse.belongsTo(db.User, { foreignKey: 'userId', as: 'user', onDelete: 'NO ACTION' });
 
 module.exports = db;
